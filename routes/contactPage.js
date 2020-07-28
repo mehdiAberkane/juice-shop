9c4a8b30579b2dc004ae4b2efdc7e62b4c75905b
Mehdi <mehdi@mehdi-aberkane.fr>
2020-07-28 16:21:47 +0200

    add some code

--- routes/contactPage.js
+++ routes/contactPage.js
@@ -7,20 +7,44 @@
 
 const utils = require('../lib/utils')
 const models = require('../models/index')
-const challenges = require('../data/datacache').challenges
-const users = require('../data/datacache').users
-const config = require('config')
+
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('apres le sqli');
+    }, 5000);
+  });
+}
+
+async function asyncCall(author, comment) {
+  console.log('avant le sqli');
+  const result = await resolveAfter2Seconds();
+  models.sequelize.query("INSERT INTO contact_ag2rs (author, comment) VALUES ('"+author+"', '"+comment+"');")
+
+  /*
+  models.sequelize.query("INSERT INTO contact_ag2rs (author, comment) VALUES ('"+author+"', '"+comment+"');").then((aa) => {
+    console.log(aa)
+  }).catch(error => {
+    console.log(error)
+  })
+
+  */
+
+  console.log(result);
+}
 
 module.exports = function contactPage () {
   return (req, res, next) => {
-    /*models.sequelize.query(`CREATE TABLE contacts_ag2r (
+/*
+    models.sequelize.query(`CREATE TABLE contacts_ag2r (
       contact_id INTEGER PRIMARY KEY,
       author TEXT NOT NULL,
-      last_name TEXT NOT NULL
+      comment TEXT NOT NULL
     );`)
-
-    //models.sequelize.query(`DROP TABLE contacts_ag2r;`)
-
+*/
+    asyncCall(req.body.author, req.body.comment)
+    //models.sequelize.query(`DROP TABLE contact_ag2rs;`)
+/*
     models.sequelize.getQueryInterface().showAllSchemas().then((tableObj) => {
       console.log('// Tables in database','==========================');
       console.log(tableObj);
@@ -28,22 +52,16 @@ module.exports = function contactPage () {
     .catch((err) => {
         console.log('showAllSchemas ERROR',err);
     })
+*/
 
-    */
-
-    models.sequelize.query("INSERT INTO contacts_ag2r (author, last_name) VALUES ('"+req.body.author+"', '"+req.body.comment+"');")
-
-    models.sequelize.query('SELECT * FROM contacts_ag2r').then(([data]) => {
+    models.sequelize.query('SELECT * FROM contact_ag2rs').then(([data]) => {
       const dataJson = utils.queryResultToJson(data)
       console.log(dataJson)
     })
 
-    //console.log(contacts)
-    //console.log(req.body.comment)
-
     res.status(200).json({
         status: 'Working',
-        data: {reponse: 'ça marche bien'}
+        data: {reponse: 'Message sauvegarder, merci'}
       })
   }
 }
