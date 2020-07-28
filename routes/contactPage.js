ddaf1bbb8d2fb40f3969efe27c3764cb88a7faab
Mehdi <mehdi@mehdi-aberkane.fr>
2020-07-28 17:22:58 +0200

    some dev tavu

--- routes/contactPage.js
+++ routes/contactPage.js
@@ -7,57 +7,49 @@
 
 const utils = require('../lib/utils')
 const models = require('../models/index')
+const fs = require('fs');
 
 function resolveAfter2Seconds() {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve('apres le sqli');
-    }, 5000);
+    }, 2000);
   });
 }
 
 async function asyncCall(author, comment) {
-  console.log('avant le sqli');
-  const result = await resolveAfter2Seconds();
-  models.sequelize.query("INSERT INTO contact_ag2rs (author, comment) VALUES ('"+author+"', '"+comment+"');")
+  const result = await resolveAfter2Seconds();  
+  models.sequelize.query("INSERT INTO contact_ag2rs (author, comment) VALUES ('"+author+"', '"+comment+"');").then((result) => {
+    models.sequelize.query('SELECT * FROM contact_ag2rs order by id desc LIMIT 1').then(([data]) => {
+      const dataJson = utils.queryResultToJson(data)
+      var xml =
+        '<?xml version="1.0" encoding="utf-8"?>' +
+        '<note importance="high" logged="true">' +
+        '    <title>'+dataJson.data[0].comment+'</title>' +
+        '    <todo>Work</todo>' +
+        '    <todo>Play</todo>' +
+        '</note>';
+        fs.writeFile('test.xml', xml, (err) => {
+          if (err) throw err;
 
-  /*
-  models.sequelize.query("INSERT INTO contact_ag2rs (author, comment) VALUES ('"+author+"', '"+comment+"');").then((aa) => {
-    console.log(aa)
+          fs.readFile('test.xml', 'utf8', function (err,data) {
+            if (err) {
+              return console.log(err);
+            }
+
+            console.log(data);
+          });
+      });
+    })
+
   }).catch(error => {
     console.log(error)
   })
-
-  */
-
-  console.log(result);
 }
 
 module.exports = function contactPage () {
   return (req, res, next) => {
-/*
-    models.sequelize.query(`CREATE TABLE contacts_ag2r (
-      contact_id INTEGER PRIMARY KEY,
-      author TEXT NOT NULL,
-      comment TEXT NOT NULL
-    );`)
-*/
     asyncCall(req.body.author, req.body.comment)
-    //models.sequelize.query(`DROP TABLE contact_ag2rs;`)
-/*
-    models.sequelize.getQueryInterface().showAllSchemas().then((tableObj) => {
-      console.log('// Tables in database','==========================');
-      console.log(tableObj);
-    })
-    .catch((err) => {
-        console.log('showAllSchemas ERROR',err);
-    })
-*/
-
-    models.sequelize.query('SELECT * FROM contact_ag2rs').then(([data]) => {
-      const dataJson = utils.queryResultToJson(data)
-      console.log(dataJson)
-    })
 
     res.status(200).json({
         status: 'Working',
