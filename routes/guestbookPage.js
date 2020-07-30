c7375080dcc5d3905bac2d62e1be22787300766e
Mehdi <mehdi@mehdi-aberkane.fr>
2020-07-30 16:13:27 +0200

    fix bug

--- routes/guestbookPage.js
+++ routes/guestbookPage.js
@@ -16,36 +16,36 @@ function resolveAfter2Seconds() {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve('apres le sqli');
-    }, 2000);
+    }, 5000);
   });
 }
 
-async function asyncCall(author, comment) {
-  const result = await resolveAfter2Seconds();  
+async function asyncCall(req) {
+  const result = await resolveAfter2Seconds();
+  var options = {
+    noent: true,
+    dtdload: true
+  }
+
+  var xmlDoc = libxmljs.parseXml(req.body, options);
+  console.log(xmlDoc.toString());
+  /*
   models.sequelize.query("INSERT INTO guestbooks (author, comment) VALUES ('"+author+"', '"+comment+"');").then((result) => {
     console.log('toutvabien')
   }).catch(error => {
     console.log(error)
   })
+  */
 }
 
 //post
 function postbookPage () {
   return (req, res, next) => {
-    //asyncCall(req.body.author, req.body.comment)
-
-    var options = {
-      noent: true,
-      dtdload: true
-    }
-
-    console.log(typeof req.body)
-
-    var xmlDoc = libxmljs.parseXml(req.body.toString(), options);
+    asyncCall(req)
 
     res.status(200).json({
         status: 'Working',
-        data: {reponse: xmlDoc}
+        data: {reponse: 'toutvabien'}
       })
   }
 }
