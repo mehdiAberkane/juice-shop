8feaa86e0f6f77e6bd69b5e2bb5db16fe24cfa90
Mehdi <mehdi@mehdi-aberkane.fr>
2020-09-03 14:54:01 +0200

    some stuff

--- routes/logger.js
+++ routes/logger.js
@@ -9,47 +9,54 @@ const utils = require('../lib/utils')
 const models = require('../models/index')
 
 function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('after');
-      }, 1000);
-    });
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('after');
+    }, 1000);
+  });
+}
+  
+async function asyncCall(req) {
+  const result = await resolveAfter2Seconds();
+  await models.logger.create({log: req.body.log });
+}
+  
+//post
+function postLogger () {
+  return (req, res, next) => {
+    asyncCall(req)
+
+    res.status(200).json({
+        status: 'Working',
+        data: {reponse: '200 ok'}
+      })
   }
-  
-  async function asyncCall(req) {
-    const result = await resolveAfter2Seconds();
-    await models.logger.create({log: req.body.log });
-  }
-  
-  //post
-  function postLogger () {
-    return (req, res, next) => {
-      asyncCall(req)
-  
-      res.status(200).json({
-          status: 'Working',
-          data: {reponse: '200 ok'}
-        })
-    }
-  }
-  
-  //get
-  function getAllLogger () {
-    return (req, res, next) => {
+}
+
+//get
+function getAllLogger () {
+  return (req, res, next) => {
+    if (typeof req.cookies.ag2r !== 'undefined' && req.cookies.ag2r == 'admin_mode') {
       models.sequelize.query("SELECT * FROM loggers").then((results) => {
         const data = utils.queryResultToJson(results)
+  
+        console.log(data.data[0])
         
-        res.status(200).json({
-          status: 'Working',
-          data: {reponse: data.data[0]}
-        })
+        res.render('logger', { title: 'Hey', message: 'Hello there!', data: data.data[0]});
+  
       }).catch(error => {
         console.log(error)
       })
+    } else {
+      res.status(404).json({
+        status: 'Not found',
+        data: {reponse: 'Not found'}
+      })
     }
   }
+}
   
-  module.exports = {
-    postLogger,
-    getAllLogger,
-  }
+module.exports = {
+  postLogger,
+  getAllLogger,
+}
